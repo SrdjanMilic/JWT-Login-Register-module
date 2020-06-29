756da648263561587e1b13aa80b61fa3430e4344
Srdjan Milic <srdjanrmilic@gmail.com>
2020-06-29 19:11:40 +0200

    upgrade to material 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,9 +16,13 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 
 // Angular Material modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
